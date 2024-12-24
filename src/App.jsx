a35d8c7c93eeb13fca6b72f9b5848dcb24f013cf
doyourbest <creative555.dev@gmail.com>
2024-12-24 00:30:13 -0600

    [update] main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import Hero from './components/home/Hero';
-import About from './components/about/About';
-import Projects from './components/projects/Projects';
-import Contact from './components/contact/Contact';
+import HomePage from './pages/HomePage';
+import ProjectDetailPage from './pages/ProjectDetailPage';
 
 function App() {
   return (
-    <MainLayout>
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-    </MainLayout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout><HomePage /></MainLayout>} />
+        <Route path="/project/:id" element={<MainLayout><ProjectDetailPage /></MainLayout>} />
+      </Routes>
+    </Router>
   );
 }
 
