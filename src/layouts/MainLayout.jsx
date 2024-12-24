dd1ca8fffb04d31e4370e720e357be15bff28e99
doyourbest <creative555.dev@gmail.com>
2024-12-24 13:03:11 -0600

    [update] main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import Header from '../components/header/Header';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
       <Header />
       {children}
     </div>
